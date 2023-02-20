80a4911f0422d4597768c42485ec04c61f5f42ba
Peter Roto <peterdroto@gmail.com>
2023-02-19 20:26:21 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const entries = require('./entries.json');
+let list = [];
+
+function main() {
+    entries.forEach(e => {
+        let entry = {
+            id: e._id,
+            user: e.user,
+            email: e.email,
+            twitter: e.twitter,
+            discord: e.discord,
+            wallet: e.wallet
+        };
+        list.push(entry);
+    });
+
+    const tableBody = document.querySelector('#entriesTable tbody');
+    list.forEach(entry => {
+        const row = document.createElement('tr');
+        Object.values(entry).forEach(value => {
+            const cell = document.createElement('td');
+            cell.textContent = value;
+            row.appendChild(cell);
+        });
+        tableBody.appendChild(row);
+    });
+}
+
+main();
